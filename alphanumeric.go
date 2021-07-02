0a231048e1c5771891f44dc3e5e4d441f92a2172
Mahbub Zulkarnain <prof.bub@live.com>
2021-07-02 11:16:21 +0700

    Update alphanumeric.go

--- alphanumeric.go
+++ alphanumeric.go
@@ -5,6 +5,6 @@ func AN(length ...int) string {
 	n := 6
 	if len(length) > 0 {
 		n = length[0]
-	}
-	return Generate(n, []rune("abcdefghijklmnopqrstuvwxyz0123456789"))
+	} 
+	return Generate(n, []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"))
 }
